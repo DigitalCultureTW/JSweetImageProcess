47a415bb5e1987b5623873327ba2834a5b192700
Jonathan <Jonathan@Zakuul>
2017-10-15 08:18:49 +0800

    finish exif orientation inf extract with basic java-js code.

--- src/main/java/tw/digitalculture/test/ExifReader.java
+++ src/main/java/tw/digitalculture/test/ExifReader.java
@@ -1,9 +1,12 @@
 package tw.digitalculture.test;
 
-import def.dom.HTMLImageElement;
-import static def.exif_js.Globals.EXIF;
+import def.dom.Blob;
+import def.dom.Event;
+import def.dom.FileReader;
+import static def.dom.Globals.alert;
+import def.dom.XMLHttpRequest;
+import def.js.DataView;
 import java.util.function.Consumer;
-import static jsweet.util.Lang.function;
 
 /**
  *
@@ -11,21 +14,67 @@ import static jsweet.util.Lang.function;
  */
 public class ExifReader {
 
-    public static void getOrientation(HTMLImageElement img, Consumer<Integer> callback) {
-        System.out.println("2");
-        System.out.println(img.src);
-        System.out.println(EXIF);
-        System.out.println(EXIF.getAllTags(img));
-        System.out.println(EXIF.getTag(img, "Orientation"));
-        System.out.println(EXIF.getTag(img, "orientation"));
-        EXIF.getData(img.src, function((o) -> {
-            System.out.println("3");
-            System.out.println("EXIF= " + EXIF);
-            System.out.println(EXIF.getTag(img, "orientation"));
-            System.out.println(EXIF.getTag(img, "Orientation"));
-            System.out.println(EXIF.getAllTags(img));
-            callback.accept((Integer) EXIF.getTag(img, "Orientation"));;
-            return null; //To change body of generated lambdas, choose Tools | Templates.
-        }));
+    public static void loadXHR(String url, Consumer<Blob> callback) {
+        try {
+            XMLHttpRequest xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.responseType = "blob";
+            xhr.onerror = (e) -> {
+                alert("Network error.");
+                return null;
+            };
+            xhr.onload = (e) -> {
+                if (xhr.status == 200) {
+                    callback.accept((Blob) xhr.response);
+                } else {
+                    alert("Loading error:" + xhr.statusText);
+                }
+                return null;
+            };
+            xhr.send();
+        } catch (Exception e) {
+            alert(e.getMessage());
+        }
+    }
+
+    public static void getOrientation(String img_path, Consumer<Integer> callback) {
+        loadXHR(img_path, (Blob img) -> {
+            FileReader reader = new FileReader();
+            reader.readAsArrayBuffer(img);
+            reader.onload = (Event e) -> {
+                DataView view = new DataView(e.target.$get("result"));
+                if (view.getUint16(0, false) != 0xFFD8) {
+                    callback.accept(-2);
+                    return null;
+                }
+                double length = view.byteLength, offset = 2;
+                while (offset < length) {
+                    double marker = view.getUint16(offset, false);
+                    offset += 2;
+                    if (marker == 0xFFE1) {
+                        if (view.getUint32(offset += 2, false) != 0x45786966) {
+                            callback.accept(-1);
+                            return null;
+                        }
+                        boolean little = view.getUint16(offset += 6, false) == 0x4949;
+                        offset += view.getUint32(offset + 4, little);
+                        double tags = view.getUint16(offset, little);
+                        offset += 2;
+                        for (int i = 0; i < tags; i++) {
+                            if (view.getUint16(offset + (i * 12), little) == 0x0112) {
+                                callback.accept((int) view.getUint16(offset + (i * 12) + 8, little));
+                                return null;
+                            }
+                        }
+                    } else if (((int) marker & 0xFF00) != 0xFF00) {
+                        break;
+                    } else {
+                        offset += view.getUint16(offset, false);
+                    }
+                }
+                callback.accept(-1);
+                return null;
+            };
+        });
     }
 }
--- src/main/java/tw/digitalculture/test/Main.java
+++ src/main/java/tw/digitalculture/test/Main.java
@@ -5,9 +5,14 @@
  */
 package tw.digitalculture.test;
 
-import static def.dom.Globals.document;
+import def.dom.Blob;
+import static def.dom.Globals.alert;
 import def.dom.HTMLImageElement;
+import def.dom.XMLHttpRequest;
+import static def.dom.Globals.document;
+import def.dom.HTMLElement;
 import static def.jquery.Globals.$;
+import java.util.function.Consumer;
 
 /**
  *
@@ -16,35 +21,72 @@ import static def.jquery.Globals.$;
 public class Main {
 
     public static void main(String[] args) {
-        $(document).ready((o) -> {
-            HTMLImageElement img = (HTMLImageElement) document.createElement("img");
-            img.src = "http://wm.localstudies.tw/element/student.jpg";
-            img.onload = (t) -> {
-                $("body").append(img);
-                $(img).css("width", 300).css("height", "auto");
-                System.out.println("1");
-                ExifReader.getOrientation(img, (Integer ori) -> {
-                    $("#ori").text(ori);
+        $(document).ready(() -> {
+//            String url = "http://data.digitalculture.tw/taichung/sites/default/files/object/photo/104%E8%87%BA%E4%B8%AD%E5%B8%82%E5%9F%8E%E4%B8%AD%E5%9F%8E%E9%A2%A8%E8%8F%AF%E9%87%8D%E7%8F%BE%20%E4%BD%B3%E4%BD%9C%20%E5%90%B3%E4%BB%81%E7%99%BC%20086-10%20%E5%8F%B0%E4%B8%AD%E8%8D%89%E6%82%9F%E9%81%93%E5%9C%B0%E6%A8%99%E4%B9%8B%E6%99%AF%E8%A7%80_0.JPG";
+//            String url = "http://wm.localstudies.tw/element/student.jpg";
+            String url = "http://data.digitalculture.tw/taichung/sites/default/files/object/photo/%E8%87%BA%E4%B8%AD%E5%B8%82%E7%A5%9E%E5%B2%A1%E5%8D%80%E5%B2%B8%E8%A3%A1%E5%9C%8B%E5%B0%8F%E5%85%A7%E7%9A%84%E5%8F%A4%E7%A2%91%E4%BA%AD%E4%B8%AD%E6%89%80%E4%BF%9D%E5%AD%98%E5%8F%A4%E7%A2%912-%E5%9C%96%EF%BC%9A%E6%B3%93%E7%BF%94.JPG";
+
+            ExifReader.getOrientation(url, (Integer ori) -> {
+                HTMLImageElement img = (HTMLImageElement) document.createElement("img");
+                img.src = url;
+                img.onload = (e) -> {
+                    String rotation = "0";
                     switch (ori) {
                         case 1:
-                            $(img).css("image-orientation", "0deg");
                             break;
                         case 3:
-                            $(img).css("image-orientation", "180deg");
+                            rotation = "180";
                             break;
                         case 6:
-                            $(img).css("image-orientation", "90deg");
+                            rotation = "90";
                             break;
                         case 8:
-                            $(img).css("image-orientation", "-90deg");
-                            break;
+                            rotation = "-90";
                     }
-//                    return null;
-                });
-                return null;
-            };
+                    $("#ori").text(ori);
+                    HTMLElement div = document.createElement("div");
+                    $(div).append(img);
+                    $(div).css("width", "300px").css("height", "300px")
+                            .css("background", "black");
+                    $(img)
+                            .css("-webkit-transform", "rotate(" + rotation + "deg)")
+                            .css("-moz-transform", "rotate(" + rotation + "deg)")
+                            .css("-ms-transform:", "rotate(" + rotation + "deg)")
+                            .css("-o-transform", "rotate(" + rotation + "deg)")
+                            .css("transform", "rotate(" + rotation + "deg)")
+                            .css("width", "100%").css("height", "100%")
+                            .css("object-fit", "contain")
+                            .css("float", "left").css("margin", "auto");
+                    $("#display").append(div);
+                    return null;
+                };
+            });
             return null;
         });
+    }
 
+    public static void loadXHR(String url, Consumer<Blob> callback) {
+        try {
+            XMLHttpRequest xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+//                xhr.withCredentials = true;
+//            xhr.setRequestHeader("Content-type", "application/xml");
+            xhr.responseType = "blob";
+            xhr.onerror = (e) -> {
+                alert("Network error.");
+                return null;
+            };
+            xhr.onload = (e) -> {
+                if (xhr.status == 200) {
+                    callback.accept((Blob) xhr.response);
+                } else {
+                    alert("Loading error:" + xhr.statusText);
+                }
+                return null;
+            };
+            xhr.send();
+        } catch (Exception e) {
+            alert(e.getMessage());
+        }
     }
 }
